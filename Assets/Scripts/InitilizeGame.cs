48d88571344b4e3aba70ddfc6e97a0f76fee42f4
yuliia <julia.a@postphysical.io>
2024-07-16 21:03:45 +0200

    initial commit

---
+++ Assets/Scripts/InitilizeGame.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Zenject;
+
+public class InitilizeGame : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Debug.Log("Initialized");
+    }
+}
---
+++ Assets/Scripts/StartMenuMV.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class StartMenuMV : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("Clicked");
+    }
+}
